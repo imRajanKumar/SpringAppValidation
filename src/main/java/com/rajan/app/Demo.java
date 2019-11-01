0da30f613170a1d71f3526a932bd11e8664eaf93
Rajan Kumar <rajank2806@gmail.com>
2019-11-01 12:55:11 +0530

    first commit

---
+++ src/main/java/com/rajan/app/Demo.java
@@ -0,0 +1,46 @@
+package com.rajan.app;
+
+
+import javax.validation.Valid;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Demo {
+	@RequestMapping("/")
+	public String say(Model m)
+	{
+		m.addAttribute("customer", new Customer());
+		return "Show";
+	}
+
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+	@RequestMapping("/processForm")
+	public String processForm(
+			@Valid @ModelAttribute("customer") Customer theCustomer,
+			BindingResult theBindingResult) {
+		
+		System.out.println("Last name: |" + theCustomer.getLastName() + "|");
+		
+		if (theBindingResult.hasErrors()) {
+			return "Show";
+		}
+		else {
+			return "hello";
+		}
+	}
+}
